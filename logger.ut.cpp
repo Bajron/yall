6aacb7cc89fdb90abce076e1d2fe6f7c33a9fd55
bkramarski <b.kramarski@gmail.com>
2017-04-29 23:12:52 +0200

    Additional tests for logger

--- logger.ut.cpp
+++ logger.ut.cpp
@@ -12,18 +12,55 @@ struct YallLoggerShould: public ::testing::Test {
   }
   std::shared_ptr<MockLoggerBackend> backendMock;
   ::yall::Logger uut;
+
+  ::yall::LoggerMessage msg;
+
+  void SetUp() {
+    EXPECT_CALL(*backendMock, take(::testing::_))
+      .Times(1).WillOnce(::testing::SaveArg<0>(&msg));
+  }
+
+  void verifyCall2() {
+    EXPECT_EQ(2, msg.sequence.size());
+    EXPECT_EQ(msg.sequence[0].value, "test");
+    EXPECT_EQ(msg.sequence[1].value, "1");
+  }
 };
 
 TEST_F(YallLoggerShould, ForwardToBackend) {
-  ::yall::LoggerMessage msg;
-
-  EXPECT_CALL(*backendMock, take(::testing::_))
-    .Times(1).WillOnce(::testing::SaveArg<0>(&msg));
-
   uut.log("test");
 
   EXPECT_EQ(1, msg.sequence.size());
   EXPECT_EQ(msg.sequence[0].value, "test");
 }
+
+TEST_F(YallLoggerShould, ForwardToBackend2) {
+  uut.log("test", 1);
+  verifyCall2();
+}
+
+TEST_F(YallLoggerShould, ForwardToBackend2WithOperator) {
+  uut("test", 1);
+  verifyCall2();
+}
+
+TEST_F(YallLoggerShould, ForwardToBackend2WithStream) {
+  uut("test", 1);
+  verifyCall2();
+}
+
+TEST_F(YallLoggerShould, ProvideMetaData) {
+  uut("test");
+  EXPECT_EQ(1, msg.meta.count("yall::TimeStamp"));
+  EXPECT_EQ(1, msg.meta.count("yall::ThreadId"));
+}
+
+TEST_F(YallLoggerShould, HandleNumbers) {
+  uut(short(1), int(1), long(1), (long long)(1),
+    (unsigned short)(1), (unsigned int)1, (unsigned long)1, (unsigned long long)1,
+    float(1), double(1));
+  EXPECT_EQ(10, msg.sequence.size());
+}
+
 
 }
